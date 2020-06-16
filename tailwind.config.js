5d3ffaa1c83050ed5bd481dd4f68a8c263f5c012
Bartosz <baart1989@gmail.com>
2020-06-16 12:53:31 +0200

    remove tailwind-theme-switcher

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,76 @@
 const plugin = require('tailwindcss/plugin');
 const _ = require('lodash');
 
+const themeSwitcher = plugin(({ addUtilities, e, theme, variants }) => {
+  console.log('themeSwitcher');
+  const themes = theme('themes');
+
+  if (!themes) {
+    console.warn('\x1b[33m', 'Warning: No themes property in your tailwind config file');
+    return;
+  }
+
+  const flattenColorPalette = require('tailwindcss/lib/util/flattenColorPalette').default;
+
+  let utilities = {};
+
+  const properties = [
+    {
+      property: 'background-color',
+      name: 'bg',
+    },
+    {
+      property: 'border-color',
+      name: 'border',
+    },
+    {
+      property: 'color',
+      name: 'placeholder',
+      pseudo: '::placeholder',
+    },
+    {
+      property: 'color',
+      name: 'text',
+    },
+  ];
+
+  Object.keys(themes).forEach(theme => {
+    const parent = e(`theme-${theme}`);
+
+    properties.forEach(prop => {
+      const res = makeColorVariant(prop, parent, theme);
+      utilities = { ...utilities, ...res };
+    });
+  });
+
+  addUtilities(utilities, ['responsive', 'hover', 'focus']);
+
+  function makeColorVariant(property, parent, th) {
+    const colors = flattenColorPalette(themes[th]);
+
+    const createPair = (p, m, v, par) => {
+      let selector;
+      if (p.pseudo) {
+        selector = `${e(`${p.name}-${m}`)}${p.pseudo}`;
+      } else {
+        selector = e(`${p.name}-${m}`);
+      }
+
+      selector = `.${par} .${selector}`;
+
+      const value = {};
+      value[p.property] = v;
+      return [selector, value];
+    };
+
+    return _.fromPairs(
+      _.map(_.omit(colors, 'default'), (value, modifier) => {
+        return createPair(property, modifier, value, parent);
+      }),
+    );
+  }
+});
+
 const gradient = plugin(({ addUtilities, e, theme, variants }) => {
   const gradients = theme('gradients', {});
   const gradientVariants = variants('gradients', []);
@@ -111,5 +181,5 @@ module.exports = {
     },
   },
   variants: {},
-  plugins: [require(`tailwind-theme-switcher`), gradient, headingSizes],
+  plugins: [themeSwitcher, gradient, headingSizes],
 };
