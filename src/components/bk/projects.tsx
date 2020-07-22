1244f542994603ba7e406e199cefea3e796a1e91
baart1989 <baart1989@gmail.com>
2020-07-22 08:37:19 +0200

    make projects section to fit in one page
    (cherry picked from commit 358f5aaaf1a7d4e3989df07eb51e1a9db4b2b403)

--- src/components/bk/projects.tsx
+++ src/components/bk/projects.tsx
@@ -13,12 +13,12 @@ export const Projects = () => {
   const projectList = projectListItems.map((text, index) => (
     <div
       key={index}
-      className="flex flex-col w-1/2 md:w-1/3 items-center justify-center h-48 md:h-64 border-medium border-b"
+      className="flex flex-col w-1/2 md:w-1/3 items-center justify-center h-32 md:h-48 border-medium border-b"
     >
-      <div className="w-12 h-12 rounded-full bg-secondary"></div>
+      <div className="w-8 h-8 rounded-full bg-secondary"></div>
       <div className="text-lg md:text-xl py-2">{text}</div>
     </div>
   ));
 
-  return <div className="flex flex-wrap py-3 px-3 md:px-0">{projectList}</div>;
+  return <div className="flex flex-wrap px-3 md:px-0">{projectList}</div>;
 };
