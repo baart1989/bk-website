e8bc5933aa888b67d76263dc6bd0e30318d05a78
baart1989 <baart1989@gmail.com>
2020-07-30 14:11:17 +0200

    avatars for employees without images

--- src/components/bk/team.tsx
+++ src/components/bk/team.tsx
@@ -31,31 +31,6 @@ const EmployeeImage = ({ image, title, name, email }) => {
   );
 };
 
-const EmployeeNoImage = ({ title, name, email }) => {
-  return (
-    <div className="m-4 flex flex-col justify-center">
-      <div
-        style={{
-          width: 272,
-        }}
-      >
-        <span className="inline-block rounded-sm overflow-hidden bg-gray-100">
-          <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
-            <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-          </svg>
-        </span>
-      </div>
-      <div className="text-center">
-        <p>{title}</p>
-        <h6>{name}</h6>
-        <a className="text-sm" href={`mailto:${email}`}>
-          {email}
-        </a>
-      </div>
-    </div>
-  );
-};
-
 export const Team = () => {
   const data: EmployeeQuery = useStaticQuery(graphql`
     query EmployeeQuery {
@@ -83,34 +58,19 @@ export const Team = () => {
     }
   `);
 
-  const teamMembers: JSX.Element[] = [];
-
-  data.items.edges.map(({ node }) => {
-    if (node.localFile) {
-      teamMembers.push(
-        <EmployeeImage
-          key={node.id}
-          title={node.title}
-          name={node.name}
-          email={node.email}
-          image={node.localFile ? node.localFile.childImageSharp.fixed : null}
-        />,
-      );
-      return;
-    }
-    teamMembers.push(
-      <EmployeeNoImage
-        key={node.id}
-        title={node.title}
-        name={node.name}
-        email={node.email}
-      ></EmployeeNoImage>,
-    );
-  });
-
   return (
     <section>
-      <div className="flex justify-center md:justify-around flex-wrap">{teamMembers}</div>
+      <div className="flex justify-center md:justify-around flex-wrap">
+        {data.items.edges.map(({ node }) => (
+          <EmployeeImage
+            key={node.id}
+            title={node.title}
+            name={node.name}
+            email={node.email}
+            image={node.localFile ? node.localFile.childImageSharp.fixed : null}
+          />
+        ))}
+      </div>
     </section>
   );
 };
